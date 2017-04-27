28fe3f1c1b60ace8d4a66dbc06b594acc3dbf98b
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-04-27 16:29:26 +0700

    MVP done

--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 const {Calculator} = require("./calculator.js")
 
 // execute function on calculator.js in here
+let calc = new Calculator(3);
+console.log(calc.add(3).substract(5).multiply(7).value);
+console.log(calc.squareRoot(16));
+console.log(calc.CircleArea(4));
