dd97e24a1dfb7d1386b0179b3e6055a49c309f93
jamessraun <jeezybig@gmail.com>
2017-04-27 16:04:27 +0700

    advanced calculator done

--- calculator.js
+++ calculator.js
@@ -2,22 +2,35 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (angka=4) {
+    this.angka=angka
   }
   add () {
+    this.angka+=2;
+    return this
   }
   substract () {
+    this.angka-=1;
+    return this
   }
   multiply () {
+    this.angka*=2;
+    return this
   }
   divide () {
+    this.angka/=1;
+    return this
   }
   square () {
+    this.angka=Math.pow(this.angka,2);
+    return this
   }
   squareRoot () {
+    return Math.sqrt(this.angka);
   }
 }
-
+var angka = new Calculator();
+console.log(angka.add().substract().multiply().divide().square().squareRoot());
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
@@ -25,6 +38,7 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+
 
 module.exports = {
   Calculator
