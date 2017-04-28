8a92708f1e4f50b80f03361c245141d036c6df6a
AndrewSenewe <sindrew.christ@gmail.com>
2017-04-28 13:11:39 +0700

    Calculator Finish

--- calculator.js
+++ calculator.js
@@ -2,19 +2,32 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num1 = 1) {
+    this.Num1 = num1;
   }
-  add () {
+  add (x) {
+    this.Num1 = this.Num1 + x;
+    return this;
   }
-  substract () {
+  substract (x) {
+    this.Num1 = this.Num1 - x;
+    return this;
   }
-  multiply () {
+  multiply (x) {
+    this.Num1 = this.Num1 * x;
+    return this;
   }
-  divide () {
+  divide (x) {
+    this.Num1 = this.Num1 / x;
+    return this;
   }
   square () {
+    this.Num1 = Math.pow(this.Num1,2);
+    return this;
   }
   squareRoot () {
+    this.Num1 = Math.sqrt(this.Num1);
+    return this;
   }
 }
 
